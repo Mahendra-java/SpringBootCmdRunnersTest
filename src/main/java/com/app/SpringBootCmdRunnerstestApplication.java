e7ea32dbde0955e1a6696cb388959eeaca1a238a
DEV01 <mahendra@mahendra-PC>
2019-06-07 17:29:30 +0530

    first Commit

---
+++ src/main/java/com/app/SpringBootCmdRunnerstestApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCmdRunnerstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCmdRunnerstestApplication.class, args);
+	}
+
+}
